ea545e308fe9df51a7e88b0cb9afcbf11b5d6301
Jagraj Singh <singh.jagraj96@yahoo.com>
2021-05-24 01:22:28 -0400

    listofquestions

--- app.js
+++ app.js
@@ -1,40 +1,77 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
+const consoleTable = require('console.table')
 // Connect connection
 const db = mysql.createConnection(
-    {
-      host: 'localhost',
-      port: 3306,
-      // Your MySQL username,
-      user: 'root',
-      // Your MySQL password
-      password: 'Bootcamp@123',
-      database: 'employees_db'
-    },
-    console.log('Connected to the employees_db database.')
-  );
+  {
+    host: 'localhost',
+    port: 3306,
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: 'Bootcamp@123',
+    database: 'employees_db'
+  },
+  console.log('Connected to the employees_db database.')
+);
 //connect to the sql server to sql db
 db.connect((err) => {
-    if (err) throw err;
-    console.log("connected to employees_db");
+  if (err) throw err;
+  console.log("connected to employees_db");
 })
 //Prompt user with list of options to choose from
 function options() {
   inquirer
-  .prompt([
-    {
-      type: 'list', message: "Welcome to the employee database! Please choose the options" , name: 'framwork', choices: [
-        'reat',
-        'node'
-      ] 
-    }
-  ])
-  .then((answers) => {
-    console.log(answer);
-  });
+    .prompt([
+      {
+        type: 'list', message: "Welcome to the employee database! Please choose the options", name: 'action', choices: [
+          'View all employees',
+          'View all departments',
+          'View all roles',
+          'Add an employee',
+          'Add a department',
+          'Add a role',
+          'Update employee role',
+          'Delete an employee',
+          'EXIT'
+        ]
+      }
+    ])
+    .then((answers) => {
+      switch (answer.action) {
+        case 'View all employees':
+          viewEmployees();
+          break;
+        case 'View all departments':
+          viewDepartments();
+          break;
+        case 'View all roles':
+          viewRoles();
+          break;
+        case 'Add an employee':
+          addEmployee();
+          break;
+        case 'Add a department':
+          addDepartment();
+          break;
+        case 'Add a role':
+          addRole();
+          break;
+        case 'Update employee role':
+          updateRole();
+          break;
+        case 'Delete an employee':
+          deleteEmployee();
+          break;
+        case 'EXIT':
+          exitApp();
+          break;
+        default:
+          break;
+      }
+      });
 
 
-  
+
 }
 
-  
