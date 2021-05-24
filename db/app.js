c27c3008bef4ecf0504d2328d99af7f84d0c056c
Jagraj Singh <singh.jagraj96@yahoo.com>
2021-05-23 22:23:39 -0400

    createdconnection

---
+++ db/app.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2');
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      port: 3306,
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'Bootcamp@123',
+      database: 'employees_db'
+    },
+    console.log('Connected to the election database.')
+  );
